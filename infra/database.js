eefc85cd33fd3a00669862a88c4834f81d39c2b1
lucastsimoes <111904606+lucastsimoes@users.noreply.github.com>
2025-03-06 04:20:05 +0000

    remove certificate.pfx

--- infra/database.js
+++ infra/database.js
@@ -10,13 +10,6 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   });
 
-  console.log("credenciais do postgres", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -39,5 +32,5 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA,
     };
   }
-  return process.env.NODE_ENV === "production" ? true : true;
+  return process.env.NODE_ENV === "development" ? false : true;
 }
