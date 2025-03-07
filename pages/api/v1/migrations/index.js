42f65622c13313ed294e3774265fa53bd83f72ec
lucastsimoes <111904606+lucastsimoes@users.noreply.github.com>
2025-03-07 23:17:48 +0000

    adds `api/v1/migrations` endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,36 @@
+import database from "infra/database.js";
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+
+export default async function migrations(req, res) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+  if (req.method === "GET") {
+    console.log("Entrou GET");
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return res.status(200).json(pendingMigrations);
+  }
+
+  if (req.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migratedMigrations.length > 0) {
+      return res.status(201).json(migratedMigrations);
+    }
+    return res.status(200).json(migratedMigrations);
+  }
+  return res.status(405).end();
+}
