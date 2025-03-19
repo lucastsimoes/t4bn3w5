f400d4abd74d9a0409874aaf0609aa9952894b1f
Lucas Simões <lucas@dlatibaia.com.br>
2025-03-19 00:04:52 -0300

    ci: fix dev

--- pages/status/index.js
+++ pages/status/index.js
@@ -19,8 +19,11 @@ function DatabaseStatus() {
     refreshInterval: 2000,
   });
 
+  let databaseStatusInfo = 'Carregando...'
+
   if (!isLoading && data) {
-    return (
+
+    databaseStatusInfo = (
       <>
         <h1>Status</h1>
         <div>Atualizado em: {data.updated_at}</div>
@@ -34,8 +37,16 @@ function DatabaseStatus() {
         </div>
         
       </>
-    );
-  } else {
-    return <div>Carregando...</div>;
+
+    )
   }
+
+  return (
+    <>
+      <div>
+        {databaseStatusInfo}
+      </div>
+    </>
+  )
+   
 }
