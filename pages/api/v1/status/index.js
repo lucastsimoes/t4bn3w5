5f0b747a6299adc934f02b01a3bb09fd91fdf784
Lucas Simões <lucas@dlatibaia.com.br>
2025-03-21 01:42:14 -0300

    fix: lint fix

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -9,29 +9,28 @@ router.get(getHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(req, res) {
-    const updatedAt = new Date().toISOString();
+  const updatedAt = new Date().toISOString();
 
-    const databaseName = process.env.POSTGRES_DB;
-    const version = await database.query("SHOW server_version;");
-    const databaseVersionValue = Number(version.rows[0].server_version);
-    const maxConnections = await database.query("SHOW max_connections;");
-    const maxConnectionsValue = Number(maxConnections.rows[0].max_connections);
+  const databaseName = process.env.POSTGRES_DB;
+  const version = await database.query("SHOW server_version;");
+  const databaseVersionValue = Number(version.rows[0].server_version);
+  const maxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = Number(maxConnections.rows[0].max_connections);
 
-    const dbOpenConnectionResult = await database.query({
-      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
-      values: [databaseName],
-    });
-    const dbOpenedConnectionValue = dbOpenConnectionResult.rows[0].count;
+  const dbOpenConnectionResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const dbOpenedConnectionValue = dbOpenConnectionResult.rows[0].count;
 
-    res.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: databaseVersionValue,
-          max_connections: maxConnectionsValue,
-          opened_connections: dbOpenedConnectionValue,
-        },
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: maxConnectionsValue,
+        opened_connections: dbOpenedConnectionValue,
       },
-    });
-
+    },
+  });
 }
