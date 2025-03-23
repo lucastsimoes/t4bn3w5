8e8ea6017373cbb7bb38be4013368d4debe0606b
Lucas Simões <lucas@dlatibaia.com.br>
2025-03-23 17:47:17 -0300

    feat: create and use `ValidationError` in `controller.js`

--- infra/controller.js
+++ infra/controller.js
@@ -1,6 +1,12 @@
-import { InternalServerError, MethodNotAllowedError } from "infra/errors";
+import { InternalServerError, MethodNotAllowedError, ValidationError } from "infra/errors";
+
 
 function onErrorHandler(error, req, res) {
+
+  if(error instanceof ValidationError) {
+    return res.status(error.statusCode).json(error);
+  }
+
   const publicErrorObject = new InternalServerError({
     statusCode: error.statusCode,
     cause: error,
