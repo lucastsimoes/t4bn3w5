41095867e550c6e507604898ec8541bbf72ac74c
Lucas Simões <lucas@dlatibaia.com.br>
2025-03-23 17:44:34 -0300

    feat: add migration to create `users` table

--- infra/migrations/1742670378615_first-migration-create-users.js
+++ infra/migrations/1742670378615_first-migration-create-users.js
@@ -1,16 +1,14 @@
-
 exports.up = (pgm) => {
-  pgm.createTable('users',{
+  pgm.createTable("users", {
     id: {
       type: "uuid",
       primaryKey: true,
-      default: pgm.func("gen_random_uuid()")
+      default: pgm.func("gen_random_uuid()"),
     },
     username: {
       type: "varchar(30)",
       notNull: true,
-      unique: true
-      
+      unique: true,
     },
     email: {
       type: "varchar(254)",
@@ -19,19 +17,17 @@ exports.up = (pgm) => {
     },
     password: {
       type: "varchar(72)",
-      notNull: true
+      notNull: true,
     },
     created_at: {
       type: "timestamptz",
-      default: pgm.func("now()")
+      default: pgm.func("now()"),
     },
     updated_at: {
       type: "timestamptz",
-      default: pgm.func("now()")
-    }
-
+      default: pgm.func("now()"),
+    },
   });
 };
-
 
 exports.down = false;
